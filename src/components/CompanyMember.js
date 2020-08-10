97a1078952dfaee72e6b205ff3ff75beaa1c5404
Roopa Rauniyar <rrauniyar@roostify.com>
2020-08-10 13:17:32 +0530

    duplicate code removed

--- src/components/CompanyMember.js
+++ src/components/CompanyMember.js
@@ -8,38 +8,41 @@ import {
   StyledOldLevelWrapper,
   StyledText,
 } from "../styles/CompanyMemberStyles";
-
-const CompanyMember = ({ member, level }) => {
-  const avatar = <MdPerson style={{ fontSize: 50 }} />;
-  const { name, designation, children } = member;
+const MemberInfo = ({ designation, name, children }) => {
   const hasLeftChildren = (children) => {
     return children && children.length && children[0];
   };
   const hasRightChildren = (children) => {
     return children && children.length && children[1];
   };
+  return (
+    <>
+      <StyledText>{designation}</StyledText>
+      <span>{name}</span>
+      <StyledArrow>
+        {hasLeftChildren(children) && <>&#8601;</>}
+        {hasRightChildren(children) && <>&#8600;</>}
+      </StyledArrow>
+    </>
+  );
+};
+
+const CompanyMember = ({ member, level }) => {
+  const avatar = <MdPerson style={{ fontSize: 50 }} />;
+  const { name, designation, children } = member;
+
   if (level % 2) {
     return (
       <StyledEvenLevelWrapper>
         <StyledEvenLevelAvatar>{avatar}</StyledEvenLevelAvatar>
-        <StyledText>{designation}</StyledText>
-        <span>{name}</span>
-        <StyledArrow>
-          {hasLeftChildren(children) && <>&#8601;</>}
-          {hasRightChildren(children) && <>&#8600;</>}
-        </StyledArrow>
+        <MemberInfo name={name} designation={designation} children={children} />
       </StyledEvenLevelWrapper>
     );
   } else {
     return (
       <StyledOldLevelWrapper>
         <StyledOddLevelAvatar>{avatar}</StyledOddLevelAvatar>
-        <StyledText>{designation}</StyledText>
-        <span>{name}</span>
-        <StyledArrow>
-          {hasLeftChildren(children) && <>&#8601;</>}
-          {hasRightChildren(children) && <>&#8600;</>}
-        </StyledArrow>
+        <MemberInfo name={name} designation={designation} children={children} />
       </StyledOldLevelWrapper>
     );
   }
